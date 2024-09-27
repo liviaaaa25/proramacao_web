3252d0be354a328c921e2cf3009a6e4d6aa25773
liviaaaa25 <santosrodrigueslivia25@gmail.com>
2024-09-27 16:22:20 -0300

    27/09

---
+++ script.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll('.hidden')
+
+//usando objeto IntersetionObserver 
+    const myObserver = new IntersectionObserver ((obs) => {
+        obs.forEach ((e) => {
+            if(e.isIntersecting){
+        //lastOne.classList.add('visivel')
+        console.log(e ,e.isIntersecting)
+        e.target.classList.add('visivel')
+    }else{
+        //lastOne.classList.remove('visivel')
+        e.target.classList.remove('visivel')
+    }
+})
+console.log('aqui!')
+    })
+elements.forEach(elemt => myObserver.observe (elemt))
